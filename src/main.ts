8bc0b71c097eca84870d4389265d43978e947210
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-03-27 21:50:21 +0200

    initial, with tailwind

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+import { setupCounter } from "./counter.ts";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <button id="counter" type="button"></button>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}, ./*.html", "./index.html"], // Add path to index.html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
