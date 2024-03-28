5d4feb1263faeff608169f7e51f75e494c9a3a49
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-03-28 09:07:38 +0200

    mobile vh

--- src/main.ts
+++ src/main.ts
@@ -15,4 +15,4 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
 
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}`);
